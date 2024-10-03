67e4db985e657a281f4d9fd0ba624ac321dc597e
MysticKim3 <83422570+MysticKim3@users.noreply.github.com>
2024-10-03 10:01:44 -0600

    Tables

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
 
 def create_tables():
-    pass
+    create_rollercoaster = """
+        CREATE TABLE Rollercoaster (
+        ID INTEGER PRIMARY KEY,
+        WaitTime INTEGER,
+        Location TEXT,
+        NumRiders INTEGER
+        ); 
+    """
+    with psycopg2.connect(conn_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute(create_rollercoaster)
+    create_guest = """
+        CREATE TABLE Guest (
+        ID INTEGER PRIMARY KEY,
+        Name TEXT,
+        RollercoasterID INT,
+        FOREIGN KEY (RollercoasterID) REFERENCES Rollercoaster(ID)
+        );
+    """
+    with psycopg2.connect(conn_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute(create_guest)
