95e17bf601e64acbd62bfc6224e2a0b13205aeaf
Sofia Iannicelli <s.iannicelli@byu.net>
2024-10-03 10:48:01 -0600

    start openai setup

--- main.py
+++ main.py
@@ -9,7 +9,25 @@ load_dotenv()
 # Access environment variables
 api_key = os.getenv("API_KEY")
 conn_string = os.getenv("CONNECTION_STRING")
+org_id = os.getenv("ORG_ID")
+
+# setup for OpenAI
+client = OpenAI(
+    organization= org_id
+)
 
 # set up tables
 setup.create_tables(conn_string)
 setup.fill_tables(conn_string)
+
+# make request
+def query_gpt(query_str):
+    response = client.chat.completions.create(
+        messages=[{
+            "role": "user",
+            "content": query_str,
+        }],
+        model="gpt-4o-mini",
+    )
+
+    return response
